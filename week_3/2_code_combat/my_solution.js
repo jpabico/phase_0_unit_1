94ef45bb666e2892036422760f2c254ca7de826d
eshem <ERIC.TOV@GMAIL.COM>
2014-05-27 17:47:53 -0400

    Update my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,14 +1,58 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+ // Rescue
  
+move right 
+move right 
+move down
+move down
+attack
  
+ // mushroom
+ up
+ right
+ left 
+ up
+ attack
  
+ // munchkins
+ right
+ attack
+ right
+ down
+ up
+ right
+ attack
  
+ // taunt
  
+ right
+ up
+ say something long
+ down
+ right
+ right
+ up
+ right
+ say
+ down
+ down
+ right
+ up 
+ right
+ 
+// its a trap 
+ down
+ down
+ say something
+ up
+ up
+ 
+ say something
  
  
  
