cefe8dba2d506dd382810f6c3ab75d9bb00e5593
jessonfoo <jessonfoo@gmail.com>
2014-05-28 15:39:56 -0700

    added my solution

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,41 +1,42 @@
-// I paired [by myself, with:] on this challenge.
+// I paired with Katia on this challenge.
 
 
 
 
 // Pseudocode
-// 
-// 
-// 
-// 
+// define secretNumber
+// define password
+// define allowedIn 
+// define members set John as member 1 and Mary as member 4
 
 
 // __________________________________________
 // Write your code below.
 
-
-
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Jesson", "Katia", "Mary"];
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
-
-
-
-
-
+//
+// This code is already in its simplest form. 
+// We simply defined the variables that need to be tested.
+// There is no need to loop, or include any logic.
+// 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
-// 
-// 
-// 
-// 
-
-
+// What we found worked for us was a sort of trial and error method. 
+// We would type out a line of code and run the file to see if it would be fixed.
+// One of the problems we faced was not knowing what we were supposed to do. Once we
+// got the ball rolling, we figured things out pretty quickly. Two questions we had when
+// working on this challenge was what assert was, and what throw was. At this point, there
+// are no real concepts we struggled with, but we learned about how the assert function works.
+// A trick that we learned was how to deal with errors in JS. I feel fairly confident with all 
+// four competencies. I enjoyed trying solve the errors in the challenge because it was like
+// solving a puzzle.
 // __________________________________________
 // Driver Test Code:  Do not alter code below this line.
 
