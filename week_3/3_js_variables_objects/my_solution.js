540194fda7461feb2d85d9b6bf0662bc45b59c2d
Eric Shemtov <shemtov.eric@gmail.com>
2014-05-29 11:38:19 -0400

    finished

--- week_3/3_js_variables_objects/my_solution.js
+++ week_3/3_js_variables_objects/my_solution.js
@@ -1,10 +1,13 @@
-// I paired [by myself, with:] on this challenge.
+// I paired [by myself] on this challenge.
 
 
 
 
 // Pseudocode
-// 
+// define a number that has to be 7
+// define a string that says just open the door
+// define allowed in as false
+// define members as a group of 4 with john being first and mary being last 
 // 
 // 
 // 
@@ -13,23 +16,28 @@
 // __________________________________________
 // Write your code below.
 
-
-
-
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Eric", "Sam", "Mary"];
 
 
 
 // __________________________________________
 // Refactored Code: Include a refactored version (or justification of your original code) here. 
 
-
+// I think the code is great. It is very simple and clean. 
+// Don't think there is any further cleaning up or clarity that can be provided here.
 
 
 
 // __________________________________________
 // Reflection: Use the reflection guidelines to write a reflection here. 
 // 
-// 
+// I think reading the instructions was the first important thing as I realized I was trying to change the tests as opposed to writing code. 
+// I did some searching on stack overflow but moreso on the tests. I found these similar to excel if statements and it therefore was pretty easy.
+// Definitely learned a bunch in satisfying the tests and putting together my first java code. I thought this was a cool activity as I have heard of
+// test based coding before. I really liked this challenge.
 // 
 // 
 // 
@@ -48,6 +56,13 @@ function assert(test, message, test_number) {
   return true;
 }
 
+var secretNumber = 7;
+var password = "just open the door";
+var allowedIn = false;
+var members = ["John", "Eric", "Sam", "Mary"];
+
+
+
 assert(
   (typeof secretNumber === 'number'),
   "The value of secretNumber should be a number.",
@@ -55,7 +70,7 @@ assert(
 )
 
 assert(
-  secretNumber === 7,
+  (secretNumber === 7),
   "The value of secretNumber should be 7.",
   "2. "
 )
