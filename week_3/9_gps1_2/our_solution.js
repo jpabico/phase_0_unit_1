5cfeaab5bcdb92dce7ef12e93ebcfe3b973d5b67
jessonfoo <jessonfoo@gmail.com>
2014-05-29 11:51:18 -0700

    update

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,8 +1,8 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Jesson Foo
+//  2. Wuang
 
 
 // 0. "YOU SIGNED... WHO?!"
@@ -19,6 +19,39 @@
 // 2. "TIME IS MONEY!"
 
 //YOUR CODE HERE!
+
+var adam ={
+  name:"Adam Sandler",
+  age: 47,
+  quote: "That's your home! Are you too good for your home?!",
+};
+var kristen={
+name: "Kristen Bell",
+age: 33,
+quote: "Do you wanna build a snowman?",
+};
+var jim={
+name:"Jim Carrey",
+age: 52,
+quote: "...So you're telling me there's a chance? YEAH!",
+};
+
+function Client(name, age, quote) {
+  this.name = name;
+  this.age = age;
+  this.quote = quote;
+  this.showInfo=function(){
+    console.log(this.name);
+    console.log(this.age);
+    console.log(this.quote);
+  };
+  this.introduction=function(){
+    console.log("my name is " + this.name + " and my age is " + this.age + " my favorite quote is " + this.quote );
+  };
+  this.showQuote=function(){
+   console.log(this.quote);
+  };
+};
 
 var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
 shooterMcGavin.constructor === Client;
